699d6f89bc3775fc8e9d9da92216380fb17016a0
Neeraj Kashyap <nkashy1@gmail.com>
2018-07-08 02:35:03 -0700

    CreateKey -> CreateKeys
    Now produces multiple accounts.
    
    Updated existing tests to reflect new signature and also added a new
    testtest to check that multiple accounts are created.
    
    Modified microcosm CLI to reflect new behaviour.

--- accounts/accounts_test.go
+++ accounts/accounts_test.go
@@ -10,8 +10,8 @@ import (
 	"testing"
 )
 
-// Tests that CreateAccount creates a JSON keyfile of the correct format when passed valid arguments
-func TestCreateAccount1(t *testing.T) {
+// Tests that CreateKeys creates a JSON keyfile of the correct format when passed valid arguments
+func TestCreateKeys1(t *testing.T) {
 	tempDir, err := ioutil.TempDir("", "microcosm")
 	if err != nil {
 		t.Fatal("Unable to create temporary directory")
@@ -20,10 +20,15 @@ func TestCreateAccount1(t *testing.T) {
 	defer os.RemoveAll(tempDir)
 
 	password := "lol"
-	address, err := CreateKey(tempDir, password)
+	addresses, err := CreateKeys(tempDir, password, 1)
 	if err != nil {
 		t.Fatal(err)
 	}
+
+	if len(addresses) != 1 {
+		t.Fatalf("Incorrect number of addresses returned -- expected: %d, actual: %d", 1, len(addresses))
+	}
+	address := addresses[0]
 
 	tempDirContents, err := ioutil.ReadDir(tempDir)
 	if err != nil {
@@ -31,7 +36,7 @@ func TestCreateAccount1(t *testing.T) {
 	}
 
 	if len(tempDirContents) != 1 {
-		t.Fatalf("Incorrect number of files in temporary directory -- expected: %d, actual %d", 1, len(tempDirContents))
+		t.Fatalf("Incorrect number of files in temporary directory -- expected: %d, actual: %d", 1, len(tempDirContents))
 	}
 
 	keyFileInfo := tempDirContents[0]
@@ -58,12 +63,67 @@ func TestCreateAccount1(t *testing.T) {
 	}
 }
 
-// Tests that CreateAccount returns an error when passed a non-existent directory as an argument
-func TestCreateAccount2(t *testing.T) {
+// Tests that CreateKeys returns an error when passed a non-existent directory as an argument
+func TestCreateKeys2(t *testing.T) {
 	keydir := "./if-you-create-a-subdirectory-with-this-name-you-are-a-jerk"
 	password := "lol"
-	_, err := CreateKey(keydir, password)
+	_, err := CreateKeys(keydir, password, 1)
 	if err == nil {
-		t.Fatalf("CreateKey call with non-existent directory did not raise an error -- expected: nil, actual: %v", err)
+		t.Fatalf("CreateKeys call with non-existent directory did not raise an error -- expected: nil, actual: %v", err)
+	}
+}
+
+// Tests that CreateKeys creates multiple keys when required to
+func TestCreateKeys3(t *testing.T) {
+	tempDir, err := ioutil.TempDir("", "microcosm")
+	if err != nil {
+		t.Fatal("Unable to create temporary directory")
+	}
+
+	defer os.RemoveAll(tempDir)
+
+	password := "lol"
+	addresses, err := CreateKeys(tempDir, password, 2)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(addresses) != 2 {
+		t.Fatalf("Incorrect number of addresses returned -- expected: %d, actual: %d", 2, len(addresses))
+	}
+
+	tempDirContents, err := ioutil.ReadDir(tempDir)
+	if err != nil {
+		t.Fatalf("Unable to list files in temporary directory: %s", tempDir)
+	}
+
+	if len(tempDirContents) != 2 {
+		t.Fatalf("Incorrect number of files in temporary directory -- expected: %d, actual: %d", 1, len(tempDirContents))
+	}
+
+	keyFileAddresses := make(map[string]bool)
+	for _, keyFileInfo := range tempDirContents {
+		keyFile := path.Join(tempDir, keyFileInfo.Name())
+		keyFileContents, err := ioutil.ReadFile(keyFile)
+		if err != nil {
+			t.Fatal(err)
+		}
+		var keyBody interface{}
+		err = json.Unmarshal(keyFileContents, &keyBody)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		storedAddress := keyBody.(map[string]interface{})["address"]
+		prefixedStoredAddressLower := strings.ToLower(fmt.Sprintf("0x%s", storedAddress))
+		keyFileAddresses[prefixedStoredAddressLower] = true
+	}
+
+	for _, address := range addresses {
+		expectedStoredAddressLower := strings.ToLower(address.String())
+		if !keyFileAddresses[expectedStoredAddressLower] {
+			t.Fatalf("Could not find address in generated keyfiles: %s", address)
+		}
 	}
 }
--- microcosm.go
+++ microcosm.go
@@ -12,7 +12,7 @@ import (
 func main() {
 	flag.Usage = func() {
 		fmt.Fprintf(os.Stderr, "Usage: %s <subcommand> [arguments]\n", os.Args[0])
-		fmt.Fprintln(os.Stderr, "Subcommands: account")
+		fmt.Fprintln(os.Stderr, "Subcommands: accounts")
 		fmt.Fprintln(os.Stderr, "For information on any <subcommand>:")
 		fmt.Fprintf(os.Stderr, "\t%s <subcommand> -h\n", os.Args[0])
 		flag.PrintDefaults()
@@ -25,20 +25,25 @@ func main() {
 	}
 
 	var keystore, password string
+	var numAccounts uint
 
 	createAccountFlags := flag.NewFlagSet("account", flag.ExitOnError)
 	createAccountFlags.StringVar(&keystore, "keystore", "./", "Directory in which to store key file")
 	createAccountFlags.StringVar(&password, "password", "microcosm", "Password with which to encrypt the key")
+	createAccountFlags.UintVar(&numAccounts, "numAccounts", 1, "Number of accounts to create")
 
 	subcommand := flag.Arg(0)
 	switch subcommand {
-	case "account":
+	case "accounts":
 		createAccountFlags.Parse(flag.Args()[1:])
-		address, err := accounts.CreateKey(keystore, password)
+		addresses, err := accounts.CreateKeys(keystore, password, numAccounts)
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Printf("Created keyfile for address: %s\n", address.String())
+		fmt.Println("Created keyfiles for the following accounts:")
+		for i, address := range addresses {
+			fmt.Printf("\t%d. %s\n", i+1, address.String())
+		}
 	default:
 		fmt.Fprintf(os.Stderr, "Invalid subcommand: %s\n", subcommand)
 		flag.Usage()
