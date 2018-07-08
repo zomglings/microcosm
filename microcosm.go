9cd24e5ea1f8890eb995e16015478a83c90c6702
Neeraj Kashyap <nkashy1@gmail.com>
2018-07-08 13:02:48 -0700

    Added subcommand for genesis file creation
    Also made `addresses` and `genesis` subcommands accept positional
    arguments through pipes in addition to directly from the command line.
    This is important for compositionality in the docker entrypoint that
    remains to be written.

--- microcosm.go
+++ microcosm.go
@@ -1,18 +1,25 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
 	"os"
+	"strings"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/nkashy1/microcosm/accounts"
+	"github.com/nkashy1/microcosm/genesis"
 )
 
 func main() {
 	flag.Usage = func() {
 		fmt.Fprintf(os.Stderr, "Usage: %s <subcommand> [arguments]\n", os.Args[0])
-		fmt.Fprintln(os.Stderr, "Subcommands: accounts, addresses")
+		fmt.Fprintln(os.Stderr, "Subcommands: accounts, addresses, genesis")
 		fmt.Fprintln(os.Stderr, "For information on any <subcommand>:")
 		fmt.Fprintf(os.Stderr, "\t%s <subcommand> -h\n", os.Args[0])
 		flag.PrintDefaults()
@@ -24,8 +31,10 @@ func main() {
 		os.Exit(1)
 	}
 
-	var keystore, password string
+	var keystore, password, genesisFile string
 	var numAccounts uint
+	var chainID, difficulty, balance int64
+	var gasLimit uint64
 
 	createAccountFlags := flag.NewFlagSet("accounts", flag.ExitOnError)
 	createAccountFlags.StringVar(&keystore, "keystore", "./", "Directory in which to store key file")
@@ -38,27 +47,86 @@ func main() {
 		getAddressesFlags.PrintDefaults()
 	}
 
+	genesisFlags := flag.NewFlagSet("genesis", flag.ExitOnError)
+	genesisFlags.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: %s, genesis -genesisFile <output path> -chainID <chain id> -difficulty <difficulty> -balance <balance> -gasLimit <gas limit> [addresses...]\n", os.Args[0])
+		genesisFlags.PrintDefaults()
+	}
+	genesisFlags.StringVar(&genesisFile, "genesisFile", "genesis.json", "Path at which to output genesis JSON file")
+	genesisFlags.Int64Var(&chainID, "chainID", 7001337, "Chain ID for private net")
+	genesisFlags.Int64Var(&difficulty, "difficulty", 100, "Chain difficulty")
+	genesisFlags.Int64Var(&balance, "balance", 10000000000, "Balance that each of the specified accounts should start with")
+	genesisFlags.Uint64Var(&gasLimit, "gasLimit", 10000000, "Gas limit for private net")
+
 	subcommand := flag.Arg(0)
+	subcommandArgs := flag.Args()[1:]
 	switch subcommand {
 	case "accounts":
-		createAccountFlags.Parse(flag.Args()[1:])
+		createAccountFlags.Parse(subcommandArgs)
 		addresses, err := accounts.CreateKeys(keystore, password, numAccounts)
 		if err != nil {
 			log.Fatal(err)
 		}
 		for _, address := range addresses {
-			fmt.Printf("%s\n", address.String())
+			fmt.Printf("%s ", address.String())
 		}
 	case "addresses":
-		getAddressesFlags.Parse(flag.Args()[1:])
-		keyFiles := getAddressesFlags.Args()
+		getAddressesFlags.Parse(subcommandArgs)
+		// If filenames have not been provided directly, check to see if they are being piped in
+		var keyFiles []string
+		keyFiles = getAddressesFlags.Args()
+		if len(keyFiles) == 0 {
+			reader := bufio.NewReader(os.Stdin)
+			pipeInput, err := reader.ReadString('\n')
+			if err != nil && err != io.EOF {
+				log.Fatal(err)
+			}
+			keyFiles = strings.Fields(pipeInput)
+		}
+
 		for _, keyFile := range keyFiles {
 			address, err := accounts.GetAddress(keyFile)
 			if err != nil {
 				log.Fatal(err)
 			}
-			fmt.Printf("%s\n", address.String())
+			fmt.Printf("%s ", address.String())
 		}
+	case "genesis":
+		genesisFlags.Parse(subcommandArgs)
+		var rawAddresses []string
+		rawAddresses = genesisFlags.Args()
+		// If addresses have not been provided directly, check to see if they are being piped in
+		if len(rawAddresses) == 0 {
+			reader := bufio.NewReader(os.Stdin)
+			pipeInput, err := reader.ReadString('\n')
+			if err != nil && err != io.EOF {
+				log.Fatal(err)
+			}
+			rawAddresses = strings.Fields(pipeInput)
+		}
+
+		addresses := make([]common.Address, len(rawAddresses))
+		for i := 0; i < len(rawAddresses); i++ {
+			addresses[i] = common.BytesToAddress([]byte(common.FromHex(rawAddresses[i])))
+		}
+
+		genesisBlock, err := genesis.CreateGenesisBlock(
+			chainID,
+			difficulty,
+			balance,
+			gasLimit,
+			addresses,
+		)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		genesisBlockJSON, err := json.Marshal(genesisBlock)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		ioutil.WriteFile(genesisFile, genesisBlockJSON, 0644)
 	default:
 		fmt.Fprintf(os.Stderr, "Invalid subcommand: %s\n", subcommand)
 		flag.Usage()
