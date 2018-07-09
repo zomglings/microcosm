c3630438597646bed08e2b25e81840b987285f69
Neeraj Kashyap <nkashy1@gmail.com>
2018-07-09 07:03:30 -0700

    Parametrized NUM_ACCOUNTS in entrypoint

--- docker/fiat-lux.sh
+++ docker/fiat-lux.sh
@@ -8,22 +8,28 @@ trap interrupt_handler INT
 
 set -e -o pipefail
 
+LOGGER="echo -e microcosm --> "
+
 DEBUG=$DEBUG
 
 set -u
 
 # If debug is set to anything nonempty, then simply run the command passed to "docker run"
 if [ ! -z $DEBUG ] ; then
+    $LOGGER "Running container in DEBUG mode with command: $@"
     $@
 else
+    NUM_ACCOUNTS=${1:-11}
+
+    $LOGGER "Creating $NUM_ACCOUNTS accounts"
     # Set up keystore with accounts and create genesis file
     KEYSTORE="/root/.ethereum/keystore"
-    NUM_ACCOUNTS=3
     PASSWORD="microcosm"
     GENESIS_FILE=/root/genesis.json
     mkdir -p $KEYSTORE
     microcosm accounts -keystore $KEYSTORE -numAccounts $NUM_ACCOUNTS -password $PASSWORD | microcosm genesis -genesisFile $GENESIS_FILE 1>/dev/null
 
+    $LOGGER "Configring geth"
     # Configure geth to use private chain
     geth init $GENESIS_FILE
 
@@ -43,8 +49,9 @@ else
     ETHERBASE=$(echo $USER_STRING | awk -F',' '{print $1}')
 
     REGULAR_ACCOUNTS=$(echo $USER_STRING | sed 's/^[^,]*,//')
-    echo "microcosm -- etherbase account: $ETHERBASE"
-    echo "microcosm -- unlocked accounts: $REGULAR_ACCOUNTS"
+    $LOGGER "etherbase account: $ETHERBASE"
+    $LOGGER "unlocked accounts: $REGULAR_ACCOUNTS"
 
+    $LOGGER "Starting geth"
     geth --mine --minerthreads 1 --unlock $USER_STRING --password $PASSWORD_FILE --etherbase $ETHERBASE
 fi
